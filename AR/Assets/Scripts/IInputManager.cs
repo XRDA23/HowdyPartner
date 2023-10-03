42326e9b010b0ac557a28e7221837da923730ae0
Lia Cicati <LiaCicati@yandex.com>
2023-10-03 11:17:54 +0200

    refactor: Reuse existing Pawn instead of PawnLia

--- AR/Assets/Scripts/IInputManager.cs
+++ AR/Assets/Scripts/IInputManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public interface IInputManager
 {
     event Action<CardTypeEnum> OnCardScanned;
-    event Action<PawnLia> OnPawnSelected;
+    event Action<PawnLogic> OnPawnSelected;
     event Action<Option> OnOptionSelected;
     event Action<Team> OnFoldAction;
     void ScanCard();  // Initiates a card scan.
-    void SelectPawn(PawnLia pawn);  // Player chooses a specific pawn.
+    void SelectPawn(PawnLogic pawn);  // Player chooses a specific pawn.
     void SelectOption(Option option);  // Player chooses an option from a card with multiple options.
     void FoldCards(Team team);  // Player chooses to fold.
 }
--- AR/Assets/Scripts/IUILogicManager.cs
+++ AR/Assets/Scripts/IUILogicManager.cs
@@ -5,11 +5,11 @@ public interface IUILogicManager
     void ShowTurn(Team team);
     void PromptForCardScan(Team team);
     void DisplayMoveOutcome(bool success, string message);
-    void DisplayPawnInEndZone(PawnLia pawn);
+    void DisplayPawnInEndZone(PawnLogic pawn);
     void DisplayPlayerCompletion(Team team);
     void AnnounceWinningTeam(Partners partners);
-    void HighlightPawn(PawnLia pawn);  // Visual feedback for selected pawn.
-    void FocusCameraOnPawn(PawnLia pawn);  // Adjust camera focus to the selected pawn.
+    void HighlightPawn(PawnLogic pawn);  // Visual feedback for selected pawn.
+    void FocusCameraOnPawn(PawnLogic pawn);  // Adjust camera focus to the selected pawn.
     void PromptCardOptionSelection(CardTypeEnum cardType);
     void ResetUIForNewTurn();
 }
