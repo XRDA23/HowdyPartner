5a0327e7d1483535d48395c3adbfabf7df9b4f81
Lia Cicati <LiaCicati@yandex.com>
2023-10-03 10:04:54 +0200

    feat: added Partners class

---
+++ AR/Assets/Scripts/Partners.cs
@@ -0,0 +1,12 @@
+﻿public class Partners
+{
+    public TeamLia Team1 { get; private set; }
+    public TeamLia Team2 { get; private set; }
+
+    public Partners(TeamLia player1, TeamLia player2)
+    {
+        Team1 = player1;
+        Team2 = player2;
+    }
+    
+}
