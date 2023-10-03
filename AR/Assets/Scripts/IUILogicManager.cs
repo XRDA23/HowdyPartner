ff0b34b0790b913b4453b76896f852a9b8bf13a1
Lia Cicati <LiaCicati@yandex.com>
2023-10-03 11:04:44 +0200

    refactor: Reuse other Team class

--- AR/Assets/Scripts/IUILogicManager.cs
+++ AR/Assets/Scripts/IUILogicManager.cs
@@ -1,10 +1,12 @@
+using Board;
+
 public interface IUILogicManager
 {
-    void ShowTurn(TeamLia team);
-    void PromptForCardScan(TeamLia team);
+    void ShowTurn(Team team);
+    void PromptForCardScan(Team team);
     void DisplayMoveOutcome(bool success, string message);
     void DisplayPawnInEndZone(PawnLia pawn);
-    void DisplayPlayerCompletion(TeamLia team);
+    void DisplayPlayerCompletion(Team team);
     void AnnounceWinningTeam(Partners partners);
     void HighlightPawn(PawnLia pawn);  // Visual feedback for selected pawn.
     void FocusCameraOnPawn(PawnLia pawn);  // Adjust camera focus to the selected pawn.
--- AR/Assets/Scripts/TeamLia.cs
+++ AR/Assets/Scripts/TeamLia.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public class TeamLia
 {
-    public PawnSymbolEnum Symbol { get; private set; }
 
-    public TeamLia(PawnSymbolEnum symbol)
-    {
-        Symbol = symbol;
-    }
 
 }
