1e41a9608e921981f2d3aab7fb3aae5bdb80c0ed
Lia Cicati <LiaCicati@yandex.com>
2023-10-03 10:26:11 +0200

    feat: Added Interface for Input Manager

---
+++ AR/Assets/Scripts/IInputManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+public interface IInputManager
+{
+    event Action<CardTypeEnum> OnCardScanned;
+    event Action<PawnLia> OnPawnSelected;
+    event Action<Option> OnOptionSelected;
+    event Action<TeamLia> OnFoldAction;
+    void ScanCard();  // Initiates a card scan.
+    void SelectPawn(PawnLia pawn);  // Player chooses a specific pawn.
+    void SelectOption(Option option);  // Player chooses an option from a card with multiple options.
+    void FoldCards(TeamLia team);  // Player chooses to fold.
+}
