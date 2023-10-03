1b508d1805878268cbd0b48d3acc2c3db1d3fbbc
Lia Cicati <LiaCicati@yandex.com>
2023-10-03 10:04:39 +0200

    feat: added Pawn class

---
+++ AR/Assets/Scripts/PawnLia.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PawnLia : MonoBehaviour 
+{
+    public TeamLia Owner { get; private set; }
+    public PawnSymbolEnum Symbol => Owner.Symbol;  // Derived from the owner's symbol.
+
+    public PawnLia(TeamLia owner)
+    {
+        Owner = owner;
+    }
+
+}
